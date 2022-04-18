48d5e627a976594b3bf107f58d1c431cdab5b9fd
angie806 <82778312+angie806@users.noreply.github.com>
2022-04-18 12:56:36 -0700

    Add files via upload

---
+++ AIAzureChatbot/State/ConversationData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AIAzureChatbot.State
+{
+    public class ConversationData
+    {
+        public bool HasUserName { get; set; } = false;
+        public bool HasUserType { get; set; } = false;
+
+        public bool DidCancel { get; set; } = false;
+        public bool IsCancellable { get; set; } = true;
+
+        public bool FailedGetUserName { get; set; } = false;
+
+        public int FailedAttemptsCount { get; set; } = 0;
+
+        public string /* [redacted] */ { get; set; } = null;
+    }
+}
---
+++ AIAzureChatbot/State/UserProfile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AIAzureChatbot.State
+{
+    public class UserProfile
+    {
+        public string Name { get; set; }
+        public bool IsShareholder { get; set; } //false -> user is an advisor
+    }
+}
