0464b845b2567c2b82aad4a5bbfbead3bbeec17a
angie806 <82778312+angie806@users.noreply.github.com>
2022-04-18 12:42:25 -0700

    Add files via upload

---
+++ AIAzureChatbot/APIModels/FundDocumentAPIModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AIAzureChatbot.APIModels
+{
+    public class FundDocumentAPIModel
+    {
+        public string DocumentType { get; set; }
+        public string FundName { get; set; }
+
+        public string KenticoCodename { get; set; } //to be used later
+        public string DocumentURL { get; set; }
+
+        public bool HasKenticoURL { get; set; } = true;
+
+        public int Year;
+        public int Month;
+        public int Quarter;
+    }
+}
